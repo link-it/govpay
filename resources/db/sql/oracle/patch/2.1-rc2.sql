196cc503963118c014b209ce4328258fa61cb282
Giovanni Bussu <bussu@link.it>
2016-05-25 17:46:11 +0200

    [GP-241] patch SQL

---
+++ resources/db/sql/oracle/patch/2.1-rc2.sql
@@ -0,0 +1,4 @@
+ALTER TABLE rpt ADD cod_transazione_rpt VARCHAR(36);
+ALTER TABLE rpt ADD cod_transazione_rt VARCHAR(36);
+ALTER TABLE rr ADD cod_transazione_rr VARCHAR(36);
+ALTER TABLE rr ADD cod_transazione_er VARCHAR(36);
