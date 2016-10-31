6f295effd01a35020c1186e6ad1786ab5d72f66d
nardil <nardi@link.it>
2016-10-31 17:14:20 +0100

    Corretto nome tipo db

--- govpay-orm/src/main/java/it/govpay/bd/nativequeries/NativeQueries.java
+++ govpay-orm/src/main/java/it/govpay/bd/nativequeries/NativeQueries.java
@@ -5,12 +5,13 @@ import org.openspcoop2.generic_project.exception.ServiceException;
 import it.govpay.bd.GovpayConfig;
 
 public abstract class NativeQueries {
+	
 	public abstract String getEstrattiContoQuery();
 	public abstract String getEstrattiContoCountQuery();
 	
 	public static NativeQueries getInstance() throws ServiceException {
 		
-		if(GovpayConfig.getInstance().getDatabaseType().equals("postgres")) {
+		if(GovpayConfig.getInstance().getDatabaseType().equals("postgresql")) {
 			return new PostgresNativeQueries();
 		}
 		if(GovpayConfig.getInstance().getDatabaseType().equals("mysql")) {
