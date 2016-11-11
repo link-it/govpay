aa513aa890148622579d0ee9fcd6dd81d51debff
nardil <nardi@link.it>
2016-11-11 12:38:31 +0100

    Aggiunte patch sql mancanti

---
+++ resources/db/sql/mysql/patch/2.2.2.sql
@@ -0,0 +1,11 @@
+--GP-393
+ALTER TABLE intermediari ADD COLUMN segregation_code INT;
+ALTER TABLE applicazioni ADD COLUMN cod_applicazione_iuv VARCHAR(3);
+ALTER TABLE domini ADD COLUMN aux_digit INT NOT NULL DEFAULT 0;
+ALTER TABLE domini ADD COLUMN iuv_prefix VARCHAR(255);
+ALTER TABLE domini ADD COLUMN iuv_prefix_strict BOOLEAN NOT NULL DEFAULT false;
+ALTER TABLE tipi_tributo ADD COLUMN tipo_contabilita VARCHAR(1);
+ALTER TABLE tipi_tributo ADD COLUMN cod_contabilita VARCHAR(255);
+ALTER TABLE tipi_tributo ADD COLUMN cod_tributo_iuv VARCHAR(255);
+ALTER TABLE tributi ADD COLUMN cod_tributo_iuv VARCHAR(255);
+
