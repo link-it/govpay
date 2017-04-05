fd7b48cfcba26587f46535f93f5f2758c930d01e
nardil <nardi@link.it>
2017-04-05 15:56:31 +0200

    Corretta inizializzazione tabella batch

--- resources/db/sql/mysql/patch/2.3.2.sql
+++ resources/db/sql/mysql/patch/2.3.2.sql
@@ -4,9 +4,9 @@ CREATE TABLE batch
        cod_batch VARCHAR(255) NOT NULL,
        nodo INT,
        -- Precisione ai millisecondi supportata dalla versione 5.6.4, se si utilizza una versione precedente non usare il suffisso '(3)'
-       inizio TIMESTAMP(3)  DEFAULT CURRENT_TIMESTAMP(3),
+       inizio TIMESTAMP(3),
        -- Precisione ai millisecondi supportata dalla versione 5.6.4, se si utilizza una versione precedente non usare il suffisso '(3)'
-       aggiornamento TIMESTAMP(3)  DEFAULT CURRENT_TIMESTAMP(3),
+       aggiornamento TIMESTAMP(3),
        -- fk/pk columns
        id BIGINT AUTO_INCREMENT,
        -- unique constraints
