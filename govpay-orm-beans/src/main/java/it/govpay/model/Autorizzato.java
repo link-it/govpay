6aaa6fd3a7e92d296e30e3e352bc00c61c32ac05
nardil <nardi@link.it>
2017-08-08 13:32:59 +0200

    aggiornamento

---
+++ govpay-orm-beans/src/main/java/it/govpay/model/Autorizzato.java
@@ -0,0 +1,23 @@
+package it.govpay.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class Autorizzato implements IAutorizzato {
+	
+	private List<Acl> acls;
+	
+	public Autorizzato() {
+		acls = new ArrayList<Acl>();
+	}
+	
+	public List<Acl> getAcls() {
+		return acls;
+	}
+	
+	public void addAllAcls(Collection<Acl> acls) {
+		acls.addAll(acls);
+	}
+
+}
