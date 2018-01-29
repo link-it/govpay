11535296a9924a4f12303981b06868ed2c9fb806
Giovanni Bussu <bussu@link.it>
2018-01-29 12:12:47 +0100

    rest services

---
+++ govpay-core/src/main/java/it/govpay/core/dao/pagamenti/dto/ListaPagamentiPortaleDTO.java
@@ -0,0 +1,59 @@
+package it.govpay.core.dao.pagamenti.dto;
+
+import java.util.Date;
+
+import it.govpay.bd.model.PagamentoPortale.STATO;
+
+public class ListaPagamentiPortaleDTO {
+
+	private String principal;
+	private Integer offset;
+	private Integer limit;
+	private Date dataA;
+	private Date dataDa;
+	private STATO stato;
+	private String versante;
+	
+	public Integer getOffset() {
+		return offset;
+	}
+	public void setOffset(Integer offset) {
+		this.offset = offset;
+	}
+	public Integer getLimit() {
+		return limit;
+	}
+	public void setLimit(Integer limit) {
+		this.limit = limit;
+	}
+	public Date getDataA() {
+		return dataA;
+	}
+	public void setDataA(Date dataA) {
+		this.dataA = dataA;
+	}
+	public Date getDataDa() {
+		return dataDa;
+	}
+	public void setDataDa(Date dataDa) {
+		this.dataDa = dataDa;
+	}
+	public STATO getStato() {
+		return stato;
+	}
+	public void setStato(STATO stato) {
+		this.stato = stato;
+	}
+	public String getVersante() {
+		return versante;
+	}
+	public void setVersante(String versante) {
+		this.versante = versante;
+	}
+	public String getPrincipal() {
+		return principal;
+	}
+	public void setPrincipal(String principal) {
+		this.principal = principal;
+	}
+}
---
+++ govpay-ejb/src/main/java/it/govpay/web/rs/v1/beans/DatiAddebito.java
@@ -0,0 +1,22 @@
+package it.govpay.web.rs.v1.beans;
+
+public class DatiAddebito {
+
+	private String ibanAddebito;
+	private String bicAddebito;
+	
+	public String getIbanAddebito() {
+		return ibanAddebito;
+	}
+	public void setIbanAddebito(String ibanAddebito) {
+		this.ibanAddebito = ibanAddebito;
+	}
+	public String getBicAddebito() {
+		return bicAddebito;
+	}
+	public void setBicAddebito(String bicAddebito) {
+		this.bicAddebito = bicAddebito;
+	}
+	
+	
+}
