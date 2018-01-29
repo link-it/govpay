c8aec29b92e4511c10d37cec2912d2f2a3775c92
pintorig <pintori@link.it>
2018-01-29 14:46:15 +0100

    Aggiunto progetto Govpay Core REST

---
+++ govpay-core-rs/src/main/java/it/govpay/rs/BaseRsService.java
@@ -0,0 +1,219 @@
+/*
+ * GovPay - Porta di Accesso al Nodo dei Pagamenti SPC 
+ * http://www.gov4j.it/govpay
+ * 
+ * Copyright (c) 2014-2017 Link.it srl (http://www.link.it).
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 3, as published by
+ * the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package it.govpay.rs;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.openspcoop2.generic_project.exception.NotFoundException;
+import org.openspcoop2.generic_project.exception.ServiceException;
+
+import it.govpay.bd.BasicBD;
+import it.govpay.bd.anagrafica.AnagraficaManager;
+import it.govpay.core.exceptions.NotAuthenticatedException;
+import it.govpay.core.utils.GpContext;
+import it.govpay.model.Applicazione;
+import it.govpay.web.handler.MessageLoggingHandlerUtils;
+import net.sf.json.JSONObject;
+
+public abstract class BaseRsService {
+
+	public static final String ERRORE_INTERNO = "Errore Interno";
+
+	@Context protected HttpServletRequest request;
+	@Context protected HttpServletResponse response;
+
+	protected String nomeServizio;
+	protected Logger log;
+
+	protected String codOperazione;
+
+	public BaseRsService(){
+		this.log = LogManager.getLogger();
+	}
+
+	public BaseRsService(String nomeServizio){
+		this();
+		this.nomeServizio = nomeServizio;
+
+	}
+
+	public void setHttpServletRequest(HttpServletRequest request) {
+		this.request = request;
+	}
+	
+
+	public void logRequest(UriInfo uriInfo, HttpHeaders rsHttpHeaders,String nomeOperazione, ByteArrayOutputStream baos) {
+		MessageLoggingHandlerUtils.logToSystemOut(uriInfo, rsHttpHeaders, this.request,baos,
+				nomeOperazione, this.nomeServizio, GpContext.TIPO_SERVIZIO_GOVPAY_JSON, getVersione(), log, false);
+	}
+	
+	public void logRequest(UriInfo uriInfo, HttpHeaders rsHttpHeaders,String nomeOperazione, byte[] baos) {
+		MessageLoggingHandlerUtils.logToSystemOut(uriInfo, rsHttpHeaders, this.request,baos,
+				nomeOperazione, this.nomeServizio, GpContext.TIPO_SERVIZIO_GOVPAY_JSON, getVersione(), log, false);
+	}
+
+	public void logResponse(UriInfo uriInfo, HttpHeaders rsHttpHeaders,String nomeOperazione, ByteArrayOutputStream baos) {
+		MessageLoggingHandlerUtils.logToSystemOut(uriInfo, rsHttpHeaders, this.request,baos,
+				nomeOperazione, this.nomeServizio, GpContext.TIPO_SERVIZIO_GOVPAY_JSON, getVersione(), log, true);
+	}
+	
+	public void logResponse(UriInfo uriInfo, HttpHeaders rsHttpHeaders,String nomeOperazione,byte[] bytes) {
+		MessageLoggingHandlerUtils.logToSystemOut(uriInfo, rsHttpHeaders, this.request,bytes,
+				nomeOperazione, this.nomeServizio, GpContext.TIPO_SERVIZIO_GOVPAY_JSON, getVersione(), log, true);
+	}
+	
+	public void logResponse(UriInfo uriInfo, HttpHeaders rsHttpHeaders,String nomeOperazione,byte[] bytes, Integer responseCode) {
+		MessageLoggingHandlerUtils.logToSystemOut(uriInfo, rsHttpHeaders, this.request,bytes,
+				nomeOperazione, this.nomeServizio, GpContext.TIPO_SERVIZIO_GOVPAY_JSON, getVersione(), log, true, responseCode);
+	}
+
+	@OPTIONS
+	@Path("{path : .*}")
+	public Response optionsAll(@PathParam("path") String path) {
+		return Response.status(Response.Status.NO_CONTENT)
+				.header("Access-Control-Allow-Origin", "*")
+				.header("Access-Control-Allow-Headers", "origin, content-type, accept, authorization")
+				.header("Access-Control-Allow-Credentials", "true")
+				.header("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST")
+				.build();
+	}
+
+	protected String getPrincipal(){
+		if(this.request.getUserPrincipal()!=null){
+			return this.request.getUserPrincipal().getName();
+		}
+		return null;
+	}
+
+	protected Response getUnauthorizedResponse(){
+		Response res =	Response.status(Response.Status.UNAUTHORIZED)
+				.header("Access-Control-Allow-Origin", "*")
+				.build();
+
+		return res;
+	}
+
+	protected Response getForbiddenResponse(){
+		Response res =	Response.status(Response.Status.FORBIDDEN)
+				.header("Access-Control-Allow-Origin", "*")
+				.build();
+
+		return res;
+	}
+
+	public static Response getBadRequestResponse(String msg){
+		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+		JSONObject jsonObject = new JSONObject();
+		jsonObject.put("esito", "KO");
+		jsonObject.put("descrizione", msg);
+
+		ByteArrayInputStream bais = new ByteArrayInputStream(jsonObject.toString().getBytes());
+		try{
+			BaseRsService.copy(bais, baos);
+
+			baos.flush();
+			baos.close();
+		}catch(Exception e){}
+
+
+		Response res =	Response.status(Response.Status.BAD_REQUEST)
+				.header("Access-Control-Allow-Origin", "*")
+				.entity(baos.toString())
+				.build();
+
+		return res;
+	}
+
+	public void invalidateSession(Logger log){
+		if(log!= null)
+			log.info("Invalidate Session in corso...");
+
+		HttpSession session = this.request.getSession(false);
+		if(session != null){
+			session.invalidate();
+		}
+
+		if(log!= null)
+			log.info("Invalidate Session completata.");
+	}
+
+	protected Applicazione getApplicazioneAutenticata(BasicBD bd) throws NotAuthenticatedException, ServiceException {
+		if(getPrincipal() == null) {
+			throw new NotAuthenticatedException();
+		}
+
+		try {
+			return AnagraficaManager.getApplicazioneByPrincipal(bd,getPrincipal());
+		} catch (NotFoundException e) {
+			throw new NotAuthenticatedException();
+		}
+	}
+
+	public static boolean isEmpty(List<?> lista){
+		if(lista == null)
+			return true;
+
+		return lista.isEmpty();
+	}
+
+	// copy method from From E.R. Harold's book "Java I/O"
+	public static void copy(InputStream in, OutputStream out) 
+			throws IOException {
+
+		// do not allow other threads to read from the
+		// input or write to the output while copying is
+		// taking place
+
+		synchronized (in) {
+			synchronized (out) {
+
+				byte[] buffer = new byte[256];
+				while (true) {
+					int bytesRead = in.read(buffer);
+					if (bytesRead == -1) break;
+					out.write(buffer, 0, bytesRead);
+				}
+			}
+		}
+	}
+	
+	public int getVersione() {
+		return 1;
+	}
+}
+
