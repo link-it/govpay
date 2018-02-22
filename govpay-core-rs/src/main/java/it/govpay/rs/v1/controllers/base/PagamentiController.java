eb56459964395a421f7c9eda44c53ac6bb40309d
pintorig <pintori@link.it>
2018-02-22 16:35:02 +0100

    in progress

--- govpay-core-rs/src/main/java/it/govpay/rs/v1/controllers/base/PagamentiController.java
+++ govpay-core-rs/src/main/java/it/govpay/rs/v1/controllers/base/PagamentiController.java
@@ -20,7 +20,7 @@ public class PagamentiController  {
         return new ResponseContext().status(Status.INTERNAL_SERVER_ERROR).entity( "Not implemented" );
     }
   */
-
+	
 
   /*
     public ResponseContext pagamentiGET(RequestContext request , Integer pagina , Integer risultatiPerPagina , String ordinamento , String campi , StatoPagamento stato , String versante , String idSessionePortale ) {
