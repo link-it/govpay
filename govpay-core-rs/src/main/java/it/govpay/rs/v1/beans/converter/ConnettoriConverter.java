69cd91d7df55a57ed434b1b3e80d9b2175e3f8bf
Giovanni Bussu <bussu@link.it>
2018-03-02 16:46:50 +0100

    in progress

--- govpay-core-rs/src/main/java/it/govpay/rs/v1/beans/converter/ConnettoriConverter.java
+++ govpay-core-rs/src/main/java/it/govpay/rs/v1/beans/converter/ConnettoriConverter.java
@@ -3,7 +3,8 @@ package it.govpay.rs.v1.beans.converter;
 import org.openspcoop2.generic_project.exception.ServiceException;
 
 import it.govpay.model.Connettore;
-import it.govpay.model.Connettore.Tipo;
+import it.govpay.model.Connettore.EnumAuthType;
+import it.govpay.model.Connettore.EnumSslType;
 import it.govpay.model.Versionabile.Versione;
 
 public class ConnettoriConverter {
@@ -11,15 +12,23 @@ public class ConnettoriConverter {
 	public static Connettore getConnettore(it.govpay.rs.v1.beans.base.Connector connector) throws ServiceException {
 		Connettore connettore = new Connettore();
 		
-		connettore.setHttpUser(connector.getAuth().getUsername());
-		connettore.setHttpPassw(connector.getAuth().getPassword());
-		connettore.setSslKsLocation(connector.getAuth().getKsLocation());
-		connettore.setSslTsLocation(connector.getAuth().getTsLocation());
-		connettore.setSslKsPasswd(connector.getAuth().getKsPassword());
-		connettore.setSslTsPasswd(connector.getAuth().getTsPassword());
-		
-		if(connector.getAuth().getTipo() != null)
-			connettore.setTipo(Tipo.valueOf(connector.getAuth().getTipo().toString()));
+		if(connector.getAuth() != null) {
+			connettore.setHttpUser(connector.getAuth().getUsername());
+			connettore.setHttpPassw(connector.getAuth().getPassword());
+			connettore.setSslKsLocation(connector.getAuth().getKsLocation());
+			connettore.setSslTsLocation(connector.getAuth().getTsLocation());
+			connettore.setSslKsPasswd(connector.getAuth().getKsPassword());
+			connettore.setSslTsPasswd(connector.getAuth().getTsPassword());
+			
+			if(connector.getAuth().getTipo() != null) {
+				connettore.setTipoAutenticazione(EnumAuthType.SSL);
+				connettore.setTipoSsl(EnumSslType.valueOf(connector.getAuth().getTipo().toString()));
+			} else {
+				connettore.setTipoAutenticazione(EnumAuthType.HTTPBasic);
+			}
+		} else {
+			connettore.setTipoAutenticazione(EnumAuthType.NONE);
+		}	
 		
 		connettore.setUrl(connector.getUrl());
 		connettore.setVersione(Versione.toEnum(connector.getVersioneApi().toString()));
