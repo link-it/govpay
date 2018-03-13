e36030dba92d5ebf9c4c1cc1ccb28e6149a75f63
pintorig <pintori@link.it>
2018-03-13 18:27:41 +0100

    corretto formato serializzazione delle date

--- govpay-core-beans/src/main/java/it/govpay/core/rs/v1/beans/base/Notifica.java
+++ govpay-core-beans/src/main/java/it/govpay/core/rs/v1/beans/base/Notifica.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Objects;
 
 import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.map.ObjectMapper;
 
 import it.gov.digitpa.schemas._2011.pagamenti.CtRicevutaTelematica;
 import it.gov.digitpa.schemas._2011.pagamenti.CtRichiestaPagamentoTelematico;
 import it.govpay.core.rs.v1.beans.JSONSerializable;
+import it.govpay.core.utils.SimpleDateFormatUtils;
 @org.codehaus.jackson.annotate.JsonPropertyOrder({
 "idA2A",
 "idPendenza",
@@ -165,6 +167,13 @@ public class Notifica extends JSONSerializable {
     }
     return o.toString().replace("\n", "\n    ");
   }
+  
+	@Override
+	public String toJSON(String fields) {
+		ObjectMapper mapper = new ObjectMapper();
+		mapper.setDateFormat(SimpleDateFormatUtils.newSimpleDateFormatSoloData());
+		return super.toJSON(fields,mapper);
+	}
 }
 
 
