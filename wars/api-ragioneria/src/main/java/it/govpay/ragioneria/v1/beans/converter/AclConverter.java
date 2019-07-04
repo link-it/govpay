3d79db54d74433575b1dbd2e2fcc1cb3b1ba4fb3
Giuliano Pintori <pintori@link.it>
2019-07-04 17:12:33 +0200

    Eliminate classi non necessarie alle API Ragioneria V1.

--- wars/api-ragioneria/src/main/java/it/govpay/ragioneria/v1/beans/converter/AclConverter.java
+++ wars/api-ragioneria/src/main/java/it/govpay/ragioneria/v1/beans/converter/AclConverter.java
@@ -3,7 +3,6 @@ package it.govpay.ragioneria.v1.beans.converter;
 import java.util.stream.Collectors;
 
 import it.govpay.ragioneria.v1.beans.AclPost;
-import it.govpay.ragioneria.v1.beans.AclPost.ServizioEnum;
 
 public class AclConverter {
 	
@@ -16,31 +15,15 @@ public class AclConverter {
 		if(acl.getServizio() != null) {
 			switch(acl.getServizio()) {
 			case ANAGRAFICA_APPLICAZIONI:
-				rsModel.setServizio(ServizioEnum.ANAGRAFICA_APPLICAZIONI);
-				break;
 			case ANAGRAFICA_CREDITORE:
-				rsModel.setServizio(ServizioEnum.ANAGRAFICA_CREDITORE);
-				break;
 			case ANAGRAFICA_PAGOPA:
-				rsModel.setServizio(ServizioEnum.ANAGRAFICA_PAGOPA);
-				break;
 			case ANAGRAFICA_RUOLI:
-				rsModel.setServizio(ServizioEnum.ANAGRAFICA_RUOLI);
-				break;
 			case CONFIGURAZIONE_E_MANUTENZIONE:
-				rsModel.setServizio(ServizioEnum.CONFIGURAZIONE_E_MANUTENZIONE);
-				break;
 			case GIORNALE_DEGLI_EVENTI:
-				rsModel.setServizio(ServizioEnum.GIORNALE_DEGLI_EVENTI);
-				break;
 			case PAGAMENTI:
-				rsModel.setServizio(ServizioEnum.PAGAMENTI);
-				break;
 			case PENDENZE:
-				rsModel.setServizio(ServizioEnum.PENDENZE);
-				break;
 			case RENDICONTAZIONI_E_INCASSI:
-				rsModel.setServizio(ServizioEnum.RENDICONTAZIONI_E_INCASSI);
+				rsModel.setServizio(acl.getServizio().getCodifica());
 				break;
 			case API_PAGAMENTI:
 			case API_PENDENZE:
