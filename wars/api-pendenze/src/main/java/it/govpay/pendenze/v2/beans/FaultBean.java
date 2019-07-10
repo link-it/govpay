c27a8feeb6e7b7c5f568c7366f6872f9a01b9e9b
Giuliano Pintori <pintori@link.it>
2019-07-10 16:23:44 +0200

    Risolto problema flusso rendicontazione non trovato quando il flusso si riferisce ad un Ente Creditore esterno.

--- wars/api-pendenze/src/main/java/it/govpay/pendenze/v2/beans/FaultBean.java
+++ wars/api-pendenze/src/main/java/it/govpay/pendenze/v2/beans/FaultBean.java
@@ -35,7 +35,7 @@ public class FaultBean extends JSONSerializable {
             
     PAGOPA("PAGOPA"),
     
-    
+            
     EC("EC"),
     
             
@@ -81,7 +81,7 @@ public class FaultBean extends JSONSerializable {
   private String dettaglio = null;
   
   /**
-   * Categoria dell'errore riscontrato:  * `AUTORIZZAZIONE` - Operazione non autorizzata  * `RICHIESTA` - Richiesta non valida  * `OPERAZIONE` - Operazione non eseguibile  * `PAGOPA` - Errore da PagoPA  * `INTERNO` - Errore interno
+   * Categoria dell'errore riscontrato:  * `AUTORIZZAZIONE` - Operazione non autorizzata  * `RICHIESTA` - Richiesta non valida  * `OPERAZIONE` - Operazione non eseguibile  * `PAGOPA` - Errore da PagoPA  * `EC` - Errore da Ente Creditore  * `INTERNO` - Errore interno
    **/
   public FaultBean categoria(CategoriaEnum categoria) {
     this.categoria = categoria;
