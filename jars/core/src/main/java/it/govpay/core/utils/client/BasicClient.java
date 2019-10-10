4112d57d00017fc7aab2a2b46192b843d9347a2d
nardil <nardi@link.it>
2019-10-10 17:44:58 +0200

    Rimossa import non utilizzata

--- jars/core/src/main/java/it/govpay/core/utils/client/BasicClient.java
+++ jars/core/src/main/java/it/govpay/core/utils/client/BasicClient.java
@@ -25,7 +25,6 @@ import java.io.IOException;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.net.URLConnection;
 import java.security.KeyStore;
 import java.util.Date;
 import java.util.HashMap;
