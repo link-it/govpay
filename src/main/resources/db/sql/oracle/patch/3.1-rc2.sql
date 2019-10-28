9d3f91578f3814309fe41922ac3d1c0f3eef48fa
Giuliano Pintori <pintori@link.it>
2019-10-28 15:51:29 +0100

    Issue #119 Aggiunta creazione della vista v_pagamenti_portale_ext alle patch sql.

--- src/main/resources/db/sql/oracle/patch/3.1-rc2.sql
+++ src/main/resources/db/sql/oracle/patch/3.1-rc2.sql
@@ -409,3 +409,49 @@ CREATE VIEW v_eventi_vers AS (
                LEFT JOIN applicazioni ON versamenti.id_applicazione = applicazioni.id
          );
 
+-- 19/09/2019 Perfezionamento configurazione standard
+UPDATE configurazione set valore = '{"apiEnte":{"letture":{"log":"SEMPRE","dump":"SOLO_ERRORE"},"scritture":{"log":"SEMPRE","dump":"SOLO_ERRORE"}},"apiPagamento":{"letture":{"log":"MAI","dump":"MAI"},"scritture":{"log":"SEMPRE","dump":"SOLO_ERRORE"}},"apiRagioneria":{"letture":{"log":"MAI","dump":"MAI"},"scritture":{"log":"SEMPRE","dump":"SOLO_ERRORE"}},"apiBackoffice":{"letture":{"log":"MAI","dump":"MAI"},"scritture":{"log":"SEMPRE","dump":"SEMPRE"}},"apiPagoPA":{"letture":{"log":"SEMPRE","dump":"SEMPRE"},"scritture":{"log":"SEMPRE","dump":"SEMPRE"}},"apiPendenze":{"letture":{"log":"MAI","dump":"MAI"},"scritture":{"log":"SEMPRE","dump":"SOLO_ERRORE"}}}' where nome='giornale_eventi';
+
+-- 23/10/2019 Vista pagamenti_portale con autorizzazione sui domini, tipi_pendenza
+
+CREATE VIEW v_pagamenti_portale_ext AS
+ SELECT 
+  pagamenti_portale.cod_canale,
+  pagamenti_portale.nome,
+  pagamenti_portale.importo,
+  pagamenti_portale.versante_identificativo,
+  pagamenti_portale.id_sessione,
+  pagamenti_portale.id_sessione_portale,
+  pagamenti_portale.id_sessione_psp,
+  pagamenti_portale.stato,
+  pagamenti_portale.codice_stato,
+  pagamenti_portale.descrizione_stato,
+  pagamenti_portale.psp_redirect_url,
+  pagamenti_portale.psp_esito,
+  pagamenti_portale.json_request,
+  pagamenti_portale.wisp_id_dominio,
+  pagamenti_portale.wisp_key_pa,
+  pagamenti_portale.wisp_key_wisp,
+  pagamenti_portale.wisp_html,
+  pagamenti_portale.data_richiesta,
+  pagamenti_portale.url_ritorno,
+  pagamenti_portale.cod_psp,
+  pagamenti_portale.tipo_versamento,
+  pagamenti_portale.multi_beneficiario,
+  pagamenti_portale.ack,
+  pagamenti_portale.tipo,
+  pagamenti_portale.principal,
+  pagamenti_portale.tipo_utenza,
+  pagamenti_portale.id,
+  pagamenti_portale.id_applicazione,
+  versamenti.id_dominio, 
+  versamenti.id_uo, 
+  versamenti.id_tipo_versamento 
+FROM pagamenti_portale 
+JOIN pag_port_versamenti ON pagamenti_portale.id = pag_port_versamenti.id_pagamento_portale 
+JOIN versamenti ON versamenti.id=pag_port_versamenti.id_versamento;
+
+
+
+
+
