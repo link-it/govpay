93200e9d8f9f646c8de3129aff8b1a6c6ae120f8
nardil <nardi@link.it>
2019-12-03 12:29:44 +0100

    Corretta generazione delle stampe degli avvisi in caso di pendenza prive di avviso

--- wars/api-backoffice/src/main/java/it/govpay/backoffice/v1/controllers/PendenzeController.java
+++ wars/api-backoffice/src/main/java/it/govpay/backoffice/v1/controllers/PendenzeController.java
@@ -1062,6 +1062,10 @@ public class PendenzeController extends BaseController {
 						idDominio =(String)map.get("idDominio");
 						numeroAvviso =(String)map.get("numeroAvviso");
 					}
+					
+					// Non tutte le pendenze caricate hanno il numero avviso
+					// In questo caso posso saltare alla successiva.
+					if(numeroAvviso == null) continue;
 
 					leggiPendenzaDTO.setIdDominio(idDominio);
 					leggiPendenzaDTO.setNumeroAvviso(numeroAvviso);
@@ -1127,6 +1131,10 @@ public class PendenzeController extends BaseController {
 							numeroAvviso =(String)map.get("numeroAvviso");
 						}
 
+						// Non tutte le pendenze caricate hanno il numero avviso
+						// In questo caso posso saltare alla successiva.
+						if(numeroAvviso == null) continue;
+						
 						leggiPendenzaDTO.setIdDominio(idDominio);
 						leggiPendenzaDTO.setNumeroAvviso(numeroAvviso);
 						LeggiPendenzaDTOResponse leggiPendenzaDTOResponse = pendenzeDAO.leggiAvvisoPagamento(leggiPendenzaDTO);
