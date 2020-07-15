4aca93a9f33d4e6f3b85879f5e7153db8cee2a6a
simo-git <>
2020-07-15 11:41:56 +0200

    Issue #220 semplificazione form caricamento tracciati pendenze - Modificato ordine campi form; - Rimosso icon-button per configurazione avanzata tracciato; - Aggiunta possibilità di personalizzare una action nel dialog-view tramite notifica

--- wars/web-console/src/main/angular/console/src/app/classes/interfaces/IFormComponent.ts
+++ wars/web-console/src/main/angular/console/src/app/classes/interfaces/IFormComponent.ts
@@ -1,9 +1,11 @@
 import { FormGroup } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 
 export interface IFormComponent {
 
   json: any;
   fGroup: FormGroup;
+  notifier?: BehaviorSubject<boolean>;
 
   verifyDisplay?(): boolean;
 
