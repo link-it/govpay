2e6aeaadd0c75b4b24c8c86264cffb8f0b8c0a4f
Giuliano Pintori <pintori@link.it>
2022-09-19 17:46:57 +0200

    Aggiunta risorsa /pendenze/byAvviso/{idDominio}/{numeroAvviso} alle API-Ragioneria V3.

--- wars/api-ragioneria/src/main/java/it/govpay/ragioneria/v3/api/PendenzeApi.java
+++ wars/api-ragioneria/src/main/java/it/govpay/ragioneria/v3/api/PendenzeApi.java
@@ -14,6 +14,7 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import it.govpay.ragioneria.v3.beans.FaultBean;
+import it.govpay.ragioneria.v3.beans.Pendenza;
 
 /**
  * GovPay - API Ragioneria
@@ -41,4 +42,22 @@ public interface PendenzeApi  {
         @ApiResponse(responseCode = "404", description = "Risorsa inesistente"),
         @ApiResponse(responseCode = "500", description = "Servizio non disponibile", content = @Content(mediaType = "application/json", schema = @Schema(implementation = FaultBean.class))) })
     public Response getAllegatoPendenza(@PathParam("id") Long id);
+
+    /**
+     * Dettaglio di una pendenza per riferimento avviso
+     *
+     * Acquisisce il dettaglio di una pendenza, comprensivo dei dati di pagamento.
+     *
+     */
+    @GET
+    @Path("/pendenze/byAvviso/{idDominio}/{numeroAvviso}")
+    @Produces({ "application/json" })
+    @Operation(summary = "Dettaglio di una pendenza per riferimento avviso", tags={ "Pendenze" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Informazioni dettagliate della pendenza", content = @Content(mediaType = "application/json", schema = @Schema(implementation = Pendenza.class))),
+        @ApiResponse(responseCode = "401", description = "Richiesta non autenticata"),
+        @ApiResponse(responseCode = "403", description = "Richiesta non autorizzata"),
+        @ApiResponse(responseCode = "404", description = "Risorsa inesistente"),
+        @ApiResponse(responseCode = "500", description = "Servizio non disponibile", content = @Content(mediaType = "application/json", schema = @Schema(implementation = FaultBean.class))) })
+    public Response getPendenzaByAvviso(@PathParam("idDominio") String idDominio, @PathParam("numeroAvviso") String numeroAvviso);
 }
