33374c4e654d3180695437cb335924d1d41f889e
Giuliano Pintori <pintori@link.it>
2023-02-09 09:40:00 +0100

    Aggiunta autorizzazione a tutti i domini e tutti i tipi pendenza per le utenze autoregistrate.

--- jars/core/src/main/java/it/govpay/core/dao/autorizzazione/AutenticazioneUtenzeRegistrateDAO.java
+++ jars/core/src/main/java/it/govpay/core/dao/autorizzazione/AutenticazioneUtenzeRegistrateDAO.java
@@ -97,6 +97,8 @@ public class AutenticazioneUtenzeRegistrateDAO extends BaseAutenticazioneDAO imp
 				}
 				utenza.setPrincipal(username);
 				utenza.setPrincipalOriginale(username);
+				utenza.setAutorizzazioneDominiStar(true);
+				utenza.setAutorizzazioneTipiVersamentoStar(true);
 				operatore.setUtenza(utenza);
 				operatoriBD.insertOperatore(operatore);
 				
