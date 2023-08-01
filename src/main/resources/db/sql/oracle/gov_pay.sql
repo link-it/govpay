410c31d514c78bf0f3e39b189268e772be8f6c77
Giuliano Pintori <pintori@link.it>
2023-08-01 17:24:39 +0200

    Eliminata patch non necessaria nella versione 3.7.x.

--- src/main/resources/db/sql/oracle/gov_pay.sql
+++ src/main/resources/db/sql/oracle/gov_pay.sql
@@ -1106,7 +1106,6 @@ CREATE INDEX idx_rpt_stato ON rpt (stato);
 CREATE INDEX idx_rpt_fk_vrs ON rpt (id_versamento);
 CREATE INDEX idx_rpt_fk_prt ON rpt (id_pagamento_portale);
 CREATE INDEX idx_rpt_data_msg_richiesta ON rpt (data_msg_richiesta);
-CREATE INDEX idx_rpt_ric_pend_scad ON rpt (cod_dominio,versione,data_msg_richiesta);
 CREATE UNIQUE INDEX idx_rpt_id_transazione ON rpt (iuv, ccp, cod_dominio);
 
 ALTER TABLE rpt MODIFY bloccante DEFAULT 1;
