5f9f03fe1e2c2a02bcd561a29e8a66d5bfd36022
Giuliano Pintori <pintori@link.it>
2023-10-10 13:56:49 +0200

    Issue #631 Aggiunta configurazione, utilizzo e test per l'autenticazione tramite API-Key in erogazione.

---
+++ jars/api-commons/src/main/java/it/govpay/rs/v1/authentication/filter/ApiKeyBasicAuthenticationConverter.java
@@ -0,0 +1,78 @@
+package it.govpay.rs.v1.authentication.filter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.openspcoop2.utils.LoggerWrapperFactory;
+import org.slf4j.Logger;
+import org.springframework.security.authentication.AuthenticationDetailsSource;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.web.authentication.AuthenticationConverter;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.authentication.www.BasicAuthenticationConverter;
+import org.springframework.util.Assert;
+
+import it.govpay.core.utils.GovpayConfig;
+
+/**
+ * Based on {@link BasicAuthenticationConverter}
+ *
+ * @author Giuliano Pintori
+ */
+public class ApiKeyBasicAuthenticationConverter implements AuthenticationConverter {
+	
+	private static Logger log = LoggerWrapperFactory.getLogger(ApiKeyBasicAuthenticationConverter.class);
+	
+	private String apiIdHeader = null;
+	private String apiKeyHeader = null;
+
+	private AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource;
+
+	public ApiKeyBasicAuthenticationConverter() {
+		this(new WebAuthenticationDetailsSource());
+	}
+
+	public ApiKeyBasicAuthenticationConverter(
+			AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource) {
+		this.authenticationDetailsSource = authenticationDetailsSource;
+		
+		this.apiIdHeader = GovpayConfig.getInstance().getAutenticazioneApiKeyNomeHeaderApiId();
+		this.apiKeyHeader = GovpayConfig.getInstance().getAutenticazioneApiKeyNomeHeaderApiKey();
+
+		if(this.apiIdHeader == null || this.apiKeyHeader == null) {
+			log.warn("Attenzione non sono stati impostati gli header dal quale leggere le informazioni per l'autenticazione API-Key");
+		}
+	}
+
+	public AuthenticationDetailsSource<HttpServletRequest, ?> getAuthenticationDetailsSource() {
+		return this.authenticationDetailsSource;
+	}
+
+	public void setAuthenticationDetailsSource(
+			AuthenticationDetailsSource<HttpServletRequest, ?> authenticationDetailsSource) {
+		Assert.notNull(authenticationDetailsSource, "AuthenticationDetailsSource required");
+		this.authenticationDetailsSource = authenticationDetailsSource;
+	}
+
+	@Override
+	public UsernamePasswordAuthenticationToken convert(HttpServletRequest request) {
+		
+		// estrazione dell'API-Id
+		log.debug("Lettura del API-Id dall'Header [{}]...", this.apiIdHeader);
+		String apiId = request.getHeader(this.apiIdHeader);
+		log.debug("Letto API-Id: [{}]", apiId);
+		
+		// estrazione dell'API-Key
+		log.debug("Lettura del API-Key dall'Header [{}]...", this.apiKeyHeader);
+		String apiKey = request.getHeader(this.apiKeyHeader);
+		log.debug("Letto API-Key: [{}]", apiKey);
+		
+		if (apiId == null || apiKey == null) {
+			return null;
+		}
+
+		UsernamePasswordAuthenticationToken result = UsernamePasswordAuthenticationToken.unauthenticated(apiId, apiKey);
+		result.setDetails(this.authenticationDetailsSource.buildDetails(request));
+		return result;
+	}
+
+}
