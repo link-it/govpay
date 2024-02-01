729cabe67945725144737658aed3225ae9bef2f6
Zulio84 <pintori@link.it>
2024-02-01 17:24:50 +0100

    Issue #672 (#687)
    Aggiunti indici mancanti alle tabelle versamenti e rendicontazioni.

--- src/main/resources/db/sql/oracle/patch/3.8.sql
+++ src/main/resources/db/sql/oracle/patch/3.8.sql
@@ -5,3 +5,11 @@ CREATE INDEX idx_rpt_ric_pend_scad ON rpt (cod_dominio,versione,data_msg_richies
 -- 12/05/2023 Aggiunti indici mancanti alla tabella eventi
 CREATE INDEX idx_evt_fk_inc ON eventi (id_incasso);
 CREATE INDEX idx_evt_fk_trac ON eventi (id_tracciato);
+
+
+-- 01/02/2024 Aggiunti indici mancanti alle tabelle versamenti e rendicontazioni
+create index idx_vrs_iuv_dominio on versamenti(iuv_versamento, id_dominio);
+create index idx_rnd_fk_singoli_versamenti on rendicontazioni(id_singolo_versamento);
+create index idx_rnd_fk_pagamenti on rendicontazioni(id_pagamento);
+
+
