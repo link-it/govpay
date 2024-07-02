1f188420a7005c4323966812dc915ece7ac63054
Luigi Buoncristiani <luigi@buoncristiani.name>
2024-07-02 10:27:57 +0200

    Fix log e cache config

--- jars/core/src/main/java/it/govpay/core/utils/CryptoUtils.java
+++ jars/core/src/main/java/it/govpay/core/utils/CryptoUtils.java
@@ -1,11 +1,14 @@
 package it.govpay.core.utils;
 
+import org.openspcoop2.utils.LoggerWrapperFactory;
 import org.openspcoop2.utils.UtilsException;
 import org.openspcoop2.utils.crypt.CryptFactory;
 import org.openspcoop2.utils.crypt.CryptType;
 import org.openspcoop2.utils.crypt.ICrypt;
 
 public class CryptoUtils {
+	private static org.slf4j.Logger log = LoggerWrapperFactory.getLogger(CryptoUtils.class);
+
 	// TODO da verificare visto che la classe Password di openspcoop2 non ha più la funzione cryptPw
 	public static String cryptPw(String plainPw) {
 		try {
@@ -19,6 +22,24 @@ public class CryptoUtils {
 
 	// TODO da verificare visto che la classe Password di openspcoop2 non ha più la funzione checkPw
 	public static boolean checkPw(String plainPw, String encodedPw) {
-		return cryptPw(plainPw).equals(encodedPw);
+		log.debug("Check password encoded: [" + encodedPw + "]");
+		String sha2CryptedPw = cryptPw(plainPw);
+		log.debug("SHA2 password encoded: [" + sha2CryptedPw + "]");
+		if (!sha2CryptedPw.equals(encodedPw)) {
+			String md5CryptedPw = cryptLibcMD5(plainPw);
+			log.debug("MD5 password encoded: [" + md5CryptedPw + "]");
+			return md5CryptedPw.equals(encodedPw);
+		}
+		return false;
+	}
+
+	private static String cryptLibcMD5(String plainPw) {
+		try {
+			ICrypt crypt = CryptFactory.getCrypt(CryptType.LIBC_CRYPT_MD5);
+			return crypt.crypt(plainPw);
+		} catch (UtilsException excp) {
+			// TODO da rivedere in base all'uso che ne viene fatto
+			throw new RuntimeException(excp);
+		}
 	}
 }
