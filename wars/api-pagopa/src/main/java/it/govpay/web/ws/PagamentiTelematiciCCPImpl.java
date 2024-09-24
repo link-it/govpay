935c9b09ede033500c67701a501bdc9fabd1ca18
Giuliano Pintori <pintori@link.it>
2024-09-24 14:30:17 +0200

    Risolte segnalazioni dipendenze librerie spring-expression e spring-web. Corretti errori nei placeholder dei messaggi diagnostici per paGetPayment e paSendRT. Corretto errore placeholder messaggio diagnostico errore in inoltro pendenza modello 4.

--- wars/api-pagopa/src/main/java/it/govpay/web/ws/PagamentiTelematiciCCPImpl.java
+++ wars/api-pagopa/src/main/java/it/govpay/web/ws/PagamentiTelematiciCCPImpl.java
@@ -943,11 +943,12 @@ public class PagamentiTelematiciCCPImpl implements PagamentiTelematiciCCP {
 		
 		String codDominio = receipt.getFiscalCode();
 		String iuv = receipt.getCreditorReferenceId();
+		String ccp = receipt.getReceiptId();
 
 		IContext ctx = ContextThreadLocal.get();
 		GpContext appContext = (GpContext) ctx.getApplicationContext();
 		
-		appContext.setCorrelationId(codDominio + iuv);
+		appContext.setCorrelationId(codDominio + iuv + ccp);
 
 		Actor from = new Actor();
 		from.setName(GpContext.NodoDeiPagamentiSPC);
@@ -961,7 +962,7 @@ public class PagamentiTelematiciCCPImpl implements PagamentiTelematiciCCP {
 
 		appContext.getRequest().addGenericProperty(new Property("codDominio", codDominio));
 		appContext.getRequest().addGenericProperty(new Property("iuv", iuv));
-		appContext.getRequest().addGenericProperty(new Property("ccp", receipt.getReceiptId()));
+		appContext.getRequest().addGenericProperty(new Property("ccp", ccp));
 
 		appContext.getEventoCtx().setCodDominio(codDominio);
 		appContext.getEventoCtx().setIuv(iuv);
@@ -973,7 +974,7 @@ public class PagamentiTelematiciCCPImpl implements PagamentiTelematiciCCP {
 			log.error("Errore durante il log dell'operazione: " + e.getMessage(),e);
 		}
 
-		log.info("Ricevuta richiesta paSendRT [" + codDominio + "][" + iuv + "]");
+		log.info("Ricevuta richiesta paSendRT [" + codDominio + "][" + iuv + "][" + ccp + "]");
 		PaSendRTRes response = new PaSendRTRes();
 
 		DatiPagoPA datiPagoPA = new DatiPagoPA();
@@ -1434,6 +1435,8 @@ public class PagamentiTelematiciCCPImpl implements PagamentiTelematiciCCP {
 			appContext.getRequest().addGenericProperty(new Property("codDominio", codDominio));
 			appContext.getRequest().addGenericProperty(new Property("iuv", iuv));
 			appContext.getRequest().addGenericProperty(new Property("ccp", ccp));
+			appContext.getRequest().addGenericProperty(new Property("codPsp", "non previsto per paGetPayment"));
+			appContext.getRequest().addGenericProperty(new Property("codCanale", "non previsto per paGetPayment"));
 			
 			try {
 				ctx.getApplicationLogger().log("ccp.ricezioneAttiva");
@@ -1918,11 +1921,12 @@ public class PagamentiTelematiciCCPImpl implements PagamentiTelematiciCCP {
 		
 		String codDominio = receipt.getFiscalCode();
 		String iuv = receipt.getCreditorReferenceId();
+		String ccp = receipt.getReceiptId();
 
 		IContext ctx = ContextThreadLocal.get();
 		GpContext appContext = (GpContext) ctx.getApplicationContext();
 		
-		appContext.setCorrelationId(codDominio + iuv);
+		appContext.setCorrelationId(codDominio + iuv + ccp);
 
 		Actor from = new Actor();
 		from.setName(GpContext.NodoDeiPagamentiSPC);
@@ -1936,7 +1940,7 @@ public class PagamentiTelematiciCCPImpl implements PagamentiTelematiciCCP {
 
 		appContext.getRequest().addGenericProperty(new Property("codDominio", codDominio));
 		appContext.getRequest().addGenericProperty(new Property("iuv", iuv));
-		appContext.getRequest().addGenericProperty(new Property("ccp", receipt.getReceiptId()));
+		appContext.getRequest().addGenericProperty(new Property("ccp", ccp));
 
 		appContext.getEventoCtx().setCodDominio(codDominio);
 		appContext.getEventoCtx().setIuv(iuv);
@@ -1948,7 +1952,7 @@ public class PagamentiTelematiciCCPImpl implements PagamentiTelematiciCCP {
 			log.error("Errore durante il log dell'operazione: " + e.getMessage(),e);
 		}
 
-		log.info("Ricevuta richiesta paSendRTV2 [" + codDominio + "][" + iuv + "]");
+		log.info("Ricevuta richiesta paSendRTV2 [" + codDominio + "][" + iuv + "][" + ccp + "]");
 		PaSendRTV2Response response = new PaSendRTV2Response();
 
 		DatiPagoPA datiPagoPA = new DatiPagoPA();
@@ -2101,6 +2105,8 @@ public class PagamentiTelematiciCCPImpl implements PagamentiTelematiciCCP {
 			appContext.getRequest().addGenericProperty(new Property("codDominio", codDominio));
 			appContext.getRequest().addGenericProperty(new Property("iuv", iuv));
 			appContext.getRequest().addGenericProperty(new Property("ccp", ccp));
+			appContext.getRequest().addGenericProperty(new Property("codPsp", "non previsto per paGetPaymentV2"));
+			appContext.getRequest().addGenericProperty(new Property("codCanale", "non previsto per paGetPaymentV2"));
 			
 			try {
 				ctx.getApplicationLogger().log("ccp.ricezioneAttiva");
