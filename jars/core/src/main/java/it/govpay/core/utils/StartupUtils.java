1a196baba649fd6e36a1c1c867df3f52dfa8a4da
Giuliano Pintori <pintori@link.it>
2025-01-23 09:30:25 +0100

    Fix procedura shutdown

--- jars/core/src/main/java/it/govpay/core/utils/StartupUtils.java
+++ jars/core/src/main/java/it/govpay/core/utils/StartupUtils.java
@@ -1,9 +1,9 @@
 /*
- * GovPay - Porta di Accesso al Nodo dei Pagamenti SPC 
+ * GovPay - Porta di Accesso al Nodo dei Pagamenti SPC
  * http://www.gov4j.it/govpay
- * 
+ *
  * Copyright (c) 2014-2025 Link.it srl (http://www.link.it).
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 3, as published by
  * the Free Software Foundation.
@@ -35,7 +35,6 @@ import java.util.TimeZone;
 import jakarta.xml.bind.JAXBException;
 
 import org.apache.commons.io.IOUtils;
-import org.apache.commons.jcs3.engine.control.CompositeCacheManager;
 import org.openspcoop2.utils.LoggerWrapperFactory;
 import org.openspcoop2.utils.UtilsException;
 import org.openspcoop2.utils.logger.LoggerFactory;
@@ -64,24 +63,24 @@ import it.govpay.pagopa.beans.utils.JaxbUtils;
 import it.govpay.stampe.utils.GovpayStampe;
 
 public class StartupUtils {
-	
+
 	private static final String MSG_ERRORE_INIZIALIZZAZIONE_DI_GOVPAY_FALLITA = "Inizializzazione di {0} fallita: {1}";
 
 	private StartupUtils() {}
 
 	private static boolean initialized = false;
 	private static boolean destroyed = false;
-	
-	public static synchronized IContext startup(Logger log, String warName, String govpayVersion, String buildVersion, 
+
+	public static synchronized IContext startup(Logger log, String warName, String govpayVersion, String buildVersion,
 			InputStream govpayPropertiesIS, URL log4j2XmlFile, InputStream msgDiagnosticiIS, String tipoServizioGovpay,
 			InputStream mappingSeveritaErroriPropertiesIS,
 			InputStream avvisiLabelPropertiesIS) throws StartupException {
-		
+
 		IContext ctx = null;
 		String versioneGovPay = getGovpayVersion(warName, govpayVersion, buildVersion);
 		String externalFileName = warName.toLowerCase();
 		if(!initialized) {
-			
+
 			GovpayConfig gpConfig = null;
 			try {
 				ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -91,7 +90,7 @@ public class StartupUtils {
 			} catch (IOException | ConfigException e) {
 				throw new StartupException(MessageFormat.format(MSG_ERRORE_INIZIALIZZAZIONE_DI_GOVPAY_FALLITA, versioneGovPay, e.getMessage()), e);
 			}
-			
+
 			// Gestione della configurazione di Log4J
 			URI log4j2Config = gpConfig.getLog4j2Config();
 			Log4jConfig log4jConfig = new Log4jConfig();
@@ -100,11 +99,11 @@ public class StartupUtils {
 			} else {
 				log4jConfig.setLog4jConfigURL(log4j2XmlFile);
 			}
-			
+
 			try {
-				log = LoggerWrapperFactory.getLogger("boot");	
-				log.info("Inizializzazione {} in corso", versioneGovPay); 
-				
+				log = LoggerWrapperFactory.getLogger("boot");
+				log.info("Inizializzazione {} in corso", versioneGovPay);
+
 				if(log4j2Config != null) {
 					log.info("Caricata configurazione logger: {}" , gpConfig.getLog4j2Config().getPath());
 				} else {
@@ -114,10 +113,10 @@ public class StartupUtils {
 			} catch (ConfigException e) {
 				throw new StartupException(MessageFormat.format(MSG_ERRORE_INIZIALIZZAZIONE_DI_GOVPAY_FALLITA, versioneGovPay, e.getMessage()), e);
 			}
-			
+
 			// inizializzo utilities di logging
 			Log4JUtils.initialize(log4j2XmlFile);
-			
+
 			// Configurazione del logger Diagnostici/Tracce/Dump
 			try {
 				DiagnosticConfig diagnosticConfig = new DiagnosticConfig();
@@ -125,14 +124,14 @@ public class StartupUtils {
 				props.load(msgDiagnosticiIS);
 				diagnosticConfig.setDiagnosticConfigProperties(props);
 				diagnosticConfig.setThrowExceptionPlaceholderFailedResolution(false);
-	
+
 				MultiLoggerConfig mConfig = new MultiLoggerConfig();
 				mConfig.setDiagnosticConfig(diagnosticConfig);
 				mConfig.setDiagnosticSeverityFilter(GovpayConfig.getInstance().getmLogLevel());
 				mConfig.setLog4jLoggerEnabled(GovpayConfig.getInstance().ismLogOnLog4j());
-				mConfig.setLog4jConfig(log4jConfig);	
+				mConfig.setLog4jConfig(log4jConfig);
 				mConfig.setDbLoggerEnabled(GovpayConfig.getInstance().ismLogOnDB());
-	
+
 				if(GovpayConfig.getInstance().ismLogOnDB()) {
 					DatabaseConfig dbConfig = new DatabaseConfig();
 					DatabaseConfigDatasource dbDSConfig = new DatabaseConfigDatasource();
@@ -143,7 +142,7 @@ public class StartupUtils {
 					mConfig.setDatabaseConfig(dbConfig);
 				}
 				LoggerFactory.initialize(GovpayConfig.getInstance().getmLogClass(), mConfig);
-	
+
 			} catch (IOException | UtilsException | ClassNotFoundException e) {
 				LogUtils.logError(log, "Errore durante la configurazione dei diagnostici", e);
 				throw new StartupException(MessageFormat.format(MSG_ERRORE_INIZIALIZZAZIONE_DI_GOVPAY_FALLITA, versioneGovPay, e.getMessage()), e);
@@ -157,7 +156,7 @@ public class StartupUtils {
 			} catch (IOException | ConfigException e) {
 				throw new StartupException(MessageFormat.format(MSG_ERRORE_INIZIALIZZAZIONE_DI_GOVPAY_FALLITA, versioneGovPay, e.getMessage()), e);
 			}
-			
+
 			// Label Avvisi Pagamento
 			try {
 				ByteArrayOutputStream baos = new ByteArrayOutputStream();
@@ -190,14 +189,14 @@ public class StartupUtils {
 				}
 			throw new StartupException(MessageFormat.format(MSG_ERRORE_INIZIALIZZAZIONE_DI_GOVPAY_FALLITA, versioneGovPay, e.getMessage()), e);
 		}
-		
+
 		initialized = true;
 		return ctx;
 	}
-	
+
 	public static synchronized IContext startupServices(Logger log, String warName, String govpayVersion, String buildVersion, IContext ctx,
 			String dominioAnagraficaManager,GovpayConfig gpConfig) throws StartupException {
-		
+
 		String versioneGovPay = getGovpayVersion(warName, govpayVersion, buildVersion);
 		try {
 			AnagraficaManager.newInstance(dominioAnagraficaManager);
@@ -213,23 +212,23 @@ public class StartupUtils {
 		log.info("TimeZone.getDefault(): {}", TimeZone.getDefault());
 		return ctx;
 	}
-	
-	
+
+
 	public static String getGovpayVersion(String warName, String govpayVersion, String buildVersion) {
 		return warName + " " + govpayVersion + " (build " + buildVersion + ")";
 	}
-	
+
 	public static synchronized void stopServices(Logger log, String warName, String govpayVersion, String buildVersion, String dominioAnagraficaManager) throws StartupException {
-		
+
 		String versioneGovPay = getGovpayVersion(warName, govpayVersion, buildVersion);
 		if(!destroyed) {
-			
+
 			log.info("Shutdown {} in corso...", versioneGovPay);
-			
+
 			log.info("De-registrazione delle cache ...");
 			AnagraficaManager.unregister();
 			log.info("De-registrazione delle cache completato");
-			
+
 			log.info("Shutdown pool thread notifiche ...");
 			try {
 				ThreadExecutorManager.shutdown();
@@ -239,13 +238,7 @@ public class StartupUtils {
 				 // Restore interrupted state...
 			    Thread.currentThread().interrupt();
 			}
-			
-			// Arresto di JCS e dei thread associati
-	        CompositeCacheManager cacheManager = CompositeCacheManager.getInstance();
-	        if (cacheManager != null) {
-	            cacheManager.shutDown();
-	        }
-			
+
 			log.info("Shutdown del Connection Manager ...");
 			try {
 				ConnectionManager.shutdown();
@@ -253,11 +246,11 @@ public class StartupUtils {
 			} catch (Exception e) {
 				log.warn("Errore nello shutdown del Connection Manager: " + e.getMessage(), e);
 			}
-			
+
 			log.info("Shutdown di {} completato.",versioneGovPay);
-			
+
 		}
-		
+
 		destroyed = true;
 	}
 }
