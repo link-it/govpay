865d847953c72e7a982d1a8c7ce9c70927ef6766
Giuliano Pintori <pintori@link.it>
2025-03-14 23:40:22 +0100

    Aggiornate istruzione per spegnere le istanze della testsuite.

--- src/main/resources/scripts/jenkins.cleanup.sh
+++ src/main/resources/scripts/jenkins.cleanup.sh
@@ -1,5 +1,5 @@
 echo "Shutdown wildfly..."
-sudo systemctl stop wildfly@govpay wildfly-26.1.3.Final@standalone
+sudo systemctl stop wildfly@govpay wildfly-26.1.3.Final@standalone tomcat_govpay
 echo "Rimozione dati del simulatore pagoPA..."
 sudo rm -rf /var/govpay-ndpsym/cache/*.cache
 sudo rm -rf /var/govpay-ndpsym/cache/RH/*
