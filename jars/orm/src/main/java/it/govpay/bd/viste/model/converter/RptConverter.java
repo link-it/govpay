e250ae36528549f50c2f0832ade92667dc8c1bf9
Giuliano Pintori <pintori@link.it>
2025-10-28 19:11:50 +0100

    Issue #818 Aggiunto campo mancante versamenti.tipo all'elenco dei field della vista v_rpt_versamenti. Corretti set versamento.id nei converter delle viste.

--- jars/orm/src/main/java/it/govpay/bd/viste/model/converter/RptConverter.java
+++ jars/orm/src/main/java/it/govpay/bd/viste/model/converter/RptConverter.java
@@ -96,7 +96,7 @@ public class RptConverter {
 		
 		Versamento versamento = new Versamento();
 
-		versamento.setId(vo.getId());
+		versamento.setId(vo.getVrsId());
 		versamento.setIdApplicazione(vo.getVrsIdApplicazione().getId());
 
 		if(vo.getVrsIdUo() != null)
