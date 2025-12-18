9f24980ecd35b5fb67b5e14b44c070778454d15d
Zulio84 <pintori@link.it>
2025-12-18 15:13:05 +0100

    Issue #833 (#834)
    Eliminati controlli di validita' semantica sul debitore in fase di acquisizione di una ricevuta.

--- jars/core/src/main/java/it/govpay/core/utils/CtReceiptUtils.java
+++ jars/core/src/main/java/it/govpay/core/utils/CtReceiptUtils.java
@@ -89,8 +89,6 @@ public class CtReceiptUtils  extends NdpValidationUtils {
 		EsitoValidazione esito = new RtUtils().new EsitoValidazione();
 		valida(ctPaymentPA.getCreditorReferenceId(), ctReceipt.getCreditorReferenceId(), esito, "CreditorReferenceId non corrisponde", true); // Identificativo di correlazione dei due messaggi lo IUV???
 
-		validaSemantica(ctPaymentPA.getDebtor(), ctReceipt.getDebtor(), esito);
-
 		StOutcome ctRecepitOutcome = ctReceipt.getOutcome(); // esito pagamento ha solo due valori OK/KO
 		String name = ctRecepitOutcome.name();
 		switch (ctRecepitOutcome) {
